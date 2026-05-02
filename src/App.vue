037d02e2b042e9eba8427fd47e9d5203ff358145
davicotico <david.ticona.saravia@gmail.com>
2026-05-02 18:19:49 -0400

    improve dark/light switch using radio button

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import { useDark } from '@vueuse/core';
 import BrushPalette from './components/BrushPalette.vue';
 import CommitsCanva from './components/CommitsCanva.vue';
 import { updateOutput1, updateOutput2 } from './utils/output';
+import IconMoon from './components/icons/IconMoon.vue';
+import IconSun from './components/icons/IconSun.vue';
 
 const isDark = useDark({
   selector: 'html',
@@ -38,13 +40,35 @@ function updateOutput() {
         <strong>Commit Pattern Painter</strong>
       </a>
       <div class="ms-auto">
-        <button
-          class="btn btn-outline-secondary"
-          @click="isDark = !isDark"
-          :title="isDark ? 'Cambiar a modo claro' : 'Cambiar a modo oscuro'"
+        <div
+          class="btn-group btn-group-sm"
+          role="group"
+          aria-label="Basic radio toggle button group"
         >
-          {{ isDark ? 'Claro' : 'Oscuro' }}
-        </button>
+          <input
+            v-model="isDark"
+            type="radio"
+            class="btn-check"
+            id="rdoDark"
+            :value="true"
+            autocomplete="off"
+          />
+          <label class="btn btn-outline-success d-flex align-items-center" for="rdoDark">
+            <icon-moon></icon-moon>
+          </label>
+
+          <input
+            v-model="isDark"
+            type="radio"
+            class="btn-check"
+            id="rdoLight"
+            :value="false"
+            autocomplete="off"
+          />
+          <label class="btn btn-outline-success d-flex align-items-center" for="rdoLight">
+            <icon-sun></icon-sun>
+          </label>
+        </div>
       </div>
     </div>
   </nav>
@@ -60,22 +84,47 @@ function updateOutput() {
       </div>
     </div>
     <div class="row">
-      <div class="col-8">
+      <div class="col-12 col-md-8">
         <div class="card">
           <div class="card-header">Output 1</div>
           <div class="card-body p-1">
             <textarea id="output1" class="form-control w-100 textarea-output"></textarea>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="row my-2">
-      <div class="col-8">
-        <div class="card">
+        <div class="card mt-2">
           <div class="card-header">Output 2</div>
           <div class="card-body p-1">
             <textarea id="output2" class="form-control w-100 textarea-output"></textarea>
           </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4 pt-3">
+        <h4>😃 Check out my projects</h4>
+        <div class="list-group mt-3">
+          <a
+            href="https://davidticona.com/quiz4learn/"
+            target="_blank"
+            class="list-group-item list-group-item-action"
+            >Quiz 4 Learn (Vue.js)</a
+          >
+          <a
+            href="https://davidticona.com/demos/javascript-menu-editor/"
+            target="_blank"
+            class="list-group-item list-group-item-action"
+            >JavaScript Menu Editor</a
+          >
+          <a
+            href="https://davidticona.com/demos/javascript-iconpicker/"
+            target="_blank"
+            class="list-group-item list-group-item-action"
+            >JavaScript Icon Picker</a
+          >
+          <a
+            href="https://davidticona.com/demos/sortablerows/"
+            target="_blank"
+            class="list-group-item list-group-item-action"
+            >JavaScript SortableRows</a
+          >
         </div>
       </div>
     </div>
